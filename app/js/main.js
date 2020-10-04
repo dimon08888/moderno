06bb8fa9a166e145166cfb07b331b8b05f063394
dimon08888 <dimon08888@mail.ru>
2020-10-04 18:52:41 +0300

    added upload

--- app/js/main.js
+++ app/js/main.js
@@ -33,16 +33,6 @@ $ (function(){
     $('.icon-th-list').removeClass('active');
   });
 
-  // $('.product-one__tabs .tabs').on('click', function (event){
-  //   var id = $(this).attr('data-id');
-  //   $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
-  //   $('.product-one__tabs .tabs').find('.tab').removeClass('active');
-  //   $(this).addClass('active');
-  //   $('#' + id).addClass('active-tab').fadeIn();
-  //   return false;
-  // });
-
-
   $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function (event) {
     var id = $(this).attr('data-id');
     $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
@@ -51,6 +41,8 @@ $ (function(){
     $('#' + id).addClass('active-tab').fadeIn();
     return false;
   });
+
+  // $('input[type="file"], select').styler();
 
  
   $('.menu__btn').on('click', function(){
